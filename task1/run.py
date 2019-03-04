941c1363a28b027c0721070512650d93bef77013
imbar <imbrkvskh@gmail.com>
2019-03-05 00:24:02 +0500

    roflanPomoika

--- task1/run.py
+++ task1/run.py
@@ -1,10 +1,19 @@
 from app import App
-import math
-
+from math import pow, e, asin
 
 if __name__ == '__main__':
-    def func(x):
-        return x*x
+    functions = dict([
+        ('1', lambda x, a, b, c: (x**2 - a**2)/(x**2 - b*x - c)),
+        ('2', lambda x, a, b, c: a*x**4/(b + x)**3),
+        ('3', lambda x, a, b, c: a*x/((b + x)*(c - x)**2)),
+        ('4', lambda x, a, b, c: ((a + x)/(b - x))**4),
+        ('5', lambda x, a, b, c: a*x/(b - x*x*x)**2),
+        ('6', lambda x, a, b, c: a*x**2/pow(x**2 - b**2, 1/3)),
+        ('7', lambda x, a, b, c: (a*x**2/(x + b))**(1/3)),
+        ('8', lambda x, a, b, c: a*e**x/(b + x)),
+        ('9', lambda x, a, b, c: asin(a)*x/(b**2 - x**2)**.5),
+        ('10', lambda x, a, b, c: a - x + (x**3/(b + x))**.5)
+    ])
 
-    app = App(func, -1, 1)
+    app = App(functions)
     app.mainloop()
