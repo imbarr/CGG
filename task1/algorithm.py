bb34f3b8b6a83c028214bd7891d910feb612d78e
imbar <imbrkvskh@gmail.com>
2019-04-01 22:35:58 +0500

    refactoring

--- task1/algorithm.py
+++ task1/algorithm.py
@@ -8,44 +8,116 @@ config.read('config.ini')
 colors = config['colors']
 
 
-def draw(width, height, func, left, right, line):
-    inf, sup = [func(left)] * 2
-    for x in np.linspace(left, right, width):
-        y = func(x)
-        if y < inf:
-            inf = y
-        if y > sup:
-            sup = y
+class Drawer:
+    def __init__(self, width, height, left, right, func, draw_line):
+        np.seterr(*(['raise']*5))
 
-    step_x = (right - left) / width
-    step_y = (sup - inf) / height
-    step = max(step_x, step_y)
-    shift_x = (1 - step_x / step) * width / 2
-    shift_y = (1 - step_y / step) * height / 2
+        self.width = width
+        self.height = height
+        self.left = left
+        self.right = right
+        self.func = func
+        self.draw_line = draw_line
 
-    to_screen_x = lambda x: shift_x + (x - left) / step
-    to_screen_y = lambda y: height - (y - inf) / step - shift_y
-    from_screen_x = lambda x: (x - shift_x) * step + left
-    from_screen_y = lambda y: (- y + height - shift_y) * step + inf
-    screen_func = lambda x: to_screen_y(func(from_screen_x(x)))
+    def draw(self):
+        self.count_bounds()
+        self.count_steps()
+        self.count_shifts()
+        self.draw_grid()
+        self.draw_axis()
+        self.draw_func()
 
-    grid_step = int(ceil(20*step))
+    def count_bounds(self):
+        self.inf, self.sup = [self.func(self.left)]*2
+        for x in np.linspace(self.left, self.right, self.width):
+            y = self.func(x)
+            if y < self.inf:
+                self.inf = y
+            elif y > self.sup:
+                self.sup = y
 
-    for x in range(0, ceil(from_screen_x(width)), grid_step):
-        line(to_screen_x(x), 0, to_screen_x(x), height, colors['grid'])
+    def count_steps(self):
+        horizontal_pixels = self.right - self.left
+        vertical_pixels = self.sup - self.inf
+        self.step_x = horizontal_pixels / self.width
+        self.step_y = vertical_pixels / self.height
+        self.step = max(self.step_x, self.step_y)
 
-    for x in range(0, ceil(from_screen_x(0)), -grid_step):
-        line(to_screen_x(x), 0, to_screen_x(x), height, colors['grid'])
+    def count_shifts(self):
+        empty_horizontal_space_share = 1 - self.step_x / self.step
+        self.shift_x = empty_horizontal_space_share * self.width / 2
+        empty_vertical_space_share = 1 - self.step_y / self.step
+        self.shift_y = empty_vertical_space_share * self.height / 2
 
-    for y in range(0, ceil(from_screen_y(height)), -grid_step):
-        line(0, to_screen_y(y), width, to_screen_y(y), colors['grid'])
+    def draw_grid(self):
+        step = self.get_grid_step()
+        left = self.from_screen_x(0)
+        right = self.from_screen_x(self.width)
+        top = self.from_screen_y(0)
+        bottom = self.from_screen_y(self.height)
 
-    for y in range(0, ceil(from_screen_y(0)), grid_step):
-        line(0, to_screen_y(y), width, to_screen_y(y), colors['grid'])
+        negative_x = np.arange(0, left, -step)
+        positive_x = np.arange(0, right, step)
+        for x in np.concatenate((negative_x, positive_x)):
+            self.draw_vertical_grid_line(x)
 
-    line(0, to_screen_y(0), width, to_screen_y(0), colors['axis'])
-    line(to_screen_x(0), 0, to_screen_x(0), height, colors['axis'])
+        negative_y = np.arange(0, bottom, -step)
+        positive_y = np.arange(0, top, step)
+        for y in np.concatenate((negative_y, positive_y)):
+            self.draw_horizontal_grid_line(y)
 
-    for x in range(int(to_screen_x(left)), int(to_screen_x(right)) + 1):
-        line(x, screen_func(x), x + 1, screen_func(x + 1), colors['function'])
+    def draw_axis(self):
+        zero_x = self.to_screen_x(0)
+        zero_y = self.to_screen_y(0)
+        self.draw_line(0, zero_y, self.width, zero_y, colors['axis'])
+        self.draw_line(zero_x, 0, zero_x, self.height, colors['axis'])
 
+    def draw_func(self):
+        screen_left = int(self.to_screen_x(self.left))
+        screen_right = int(ceil(self.to_screen_x(self.right)))
+        for x in range(screen_left, screen_right):
+            self.draw_line(x, self.screen_func(x), x + 1, self.screen_func(x + 1), colors['function'])
+
+    def to_screen_x(self, x):
+        length = x - self.left
+        return self.shift_x + self.to_screen_length(length)
+
+    def to_screen_y(self, y):
+        length = y - self.inf
+        flipped_screen_y = self.shift_y + self.to_screen_length(length)
+        return self.height - flipped_screen_y
+
+    def from_screen_x(self, x):
+        screen_length = x - self.shift_x
+        length = self.from_screen_length(screen_length)
+        return self.left + length
+
+    def from_screen_y(self, y):
+        screen_length = y - self.shift_y
+        flipped_screen_length = self.height - screen_length
+        length = self.from_screen_length(flipped_screen_length)
+        return self.inf + length
+
+    def to_screen_length(self, length):
+        return length / self.step
+
+    def from_screen_length(self, length):
+        return length * self.step
+
+    def get_grid_step(self):
+        # TODO: change alg
+        return int(ceil(20*self.step))
+
+    def draw_vertical_grid_line(self, x):
+        screen_x = self.to_screen_x(x)
+        self.draw_line(screen_x, 0, screen_x, self.height, colors['grid'])
+
+    def draw_horizontal_grid_line(self, y):
+        screen_y = self.to_screen_y(y)
+        self.draw_line(0, screen_y, self.width, screen_y, colors['grid'])
+
+    def screen_func(self, screen_x):
+        x = self.from_screen_x(screen_x)
+        y = self.func(x)
+        screen_y = self.to_screen_y(y)
+        return screen_y
--- task1/app.py
+++ task1/app.py
@@ -1,6 +1,7 @@
-from tkinter import Frame, OptionMenu, Entry, StringVar, DoubleVar, Canvas, BOTH, YES, END, Tk, Label, S, N, E, W, TclError
+from tkinter import Frame, OptionMenu, Entry, StringVar, DoubleVar, Canvas, Tk, Label, TclError
+import tkinter as tk
 from threading import Thread
-from algorithm import draw
+from algorithm import Drawer
 import configparser
 
 config = configparser.ConfigParser()
@@ -11,90 +12,89 @@ app = config['app']
 class App(Tk):
     def __init__(self, functions, master=None, **kw):
         super().__init__(master, **kw)
+        self.functions = functions
+
         self.configure(background=app['theme'])
-        f = Frame(self, background=app['theme'])
-        f.pack(fill=BOTH, expand=YES, padx=10, pady=10)
-        f.rowconfigure(7, weight=1)
-        f.columnconfigure(3, weight=1)
+        wrapper = Frame(self, background=app['theme'])
+        wrapper.pack(fill=tk.BOTH, expand=tk.YES, padx=10, pady=10)
+        wrapper.rowconfigure(7, weight=1)
+        wrapper.columnconfigure(3, weight=1)
 
-        Label(f, text='Функция', background=app['theme']).grid(row=1, column=1, sticky=E, padx=10, pady=5)
-        func = StringVar(f)
-        func.set(list(functions)[0])
-        func_entry = OptionMenu(f, func, *list(functions))
-        func_entry.configure(borderwidth=0)
-        func_entry.grid(row=1, column=2, sticky=W+E)
-
-        Label(f, text='Левая граница', background=app['theme']).grid(row=2, column=1, sticky=E, padx=10, pady=5)
-        left = DoubleVar(f)
-        left.set(-0.1)
-        left_entry = Entry(f, textvariable=left)
-        left_entry.grid(row=2, column=2, sticky=W)
-
-        Label(f, text='Правая граница', background=app['theme']).grid(row=3, column=1, sticky=E, padx=10, pady=5)
-        right = DoubleVar(f)
-        right.set(0.1)
-        right_entry = Entry(f, textvariable=right)
-        right_entry.grid(row=3, column=2, sticky=W)
-
-        Label(f, text='a', background=app['theme']).grid(row=4, column=1, sticky=E, padx=10, pady=5)
-        a_var = DoubleVar(f)
-        a_var.set(1)
-        a_entry = Entry(f, textvariable=a_var)
-        a_entry.grid(row=4, column=2, sticky=W)
-
-        Label(f, text='b', background=app['theme']).grid(row=5, column=1, sticky=E, padx=10, pady=5)
-        b_var = DoubleVar(f)
-        b_var.set(1)
-        b_entry = Entry(f, textvariable=b_var)
-        b_entry.grid(row=5, column=2, sticky=W)
-
-        Label(f, text='c', background=app['theme']).grid(row=6, column=1, sticky=E, padx=10, pady=5)
-        c_var = DoubleVar(f)
-        c_var.set(1)
-        c_entry = Entry(f, textvariable=c_var)
-        c_entry.grid(row=6, column=2, sticky=W)
-
-        canvas_wrapper = Frame(f, background=app['canvas'])
-        canvas_wrapper.grid(row=7, column=1, columnspan=3, sticky=N+S+W+E)
-        self.canvas = Canvas(canvas_wrapper, borderwidth=0, highlightthickness=0, background=app['canvas'])
-        self.canvas.pack(fill=BOTH, expand=YES)
-
-        def draw_all(*args):
-            try:
-                a, b, c = a_var.get(), b_var.get(), c_var.get()
-                l, r = left.get(), right.get()
-                function = functions[func.get()]
-
-                def reduced(x):
-                    return function(x, a, b, c)
-
-                width, height = self.canvas.winfo_width(), self.canvas.winfo_height()
-                self.async_draw(width, height, reduced, l, r, self.line)
-            except TclError:
-                pass
-
-        self.canvas.bind("<Configure>", draw_all)
-
-        for widget in [func, left, right, a_var, b_var, c_var]:
-            widget.trace("w", draw_all)
-
+        self.create_entries(wrapper)
+        self.create_canvas(wrapper)
+        self.bind_redraw_events()
         self.drawing = False
 
-    def line(self, x1, y1, x2, y2, color):
-        self.canvas.create_line(x1, y1, x2, y2, fill=color)
+    def create_entries(self, master):
+        self.func = App.create_function_entry(master, self.functions)
+        self.left = App.create_real_entry(master, text='Левая граница', row=2, initial_value=-0.1)
+        self.right = App.create_real_entry(master, text='Правая граница', row=3, initial_value=0.1)
+        self.a = App.create_real_entry(master, text='a', row=4, initial_value=1)
+        self.b = App.create_real_entry(master, text='b', row=5, initial_value=1)
+        self.c = App.create_real_entry(master, text='c', row=6, initial_value=1)
 
-    def async_draw(self, *args):
-        def draw_with_flags():
+    def create_canvas(self, master):
+        canvas_wrapper = Frame(master, background=app['canvas'])
+        canvas_wrapper.grid(row=7, column=1, columnspan=3, sticky=tk.N + tk.S + tk.W + tk.E)
+        self.canvas = Canvas(canvas_wrapper, borderwidth=0, highlightthickness=0, background=app['canvas'])
+        self.canvas.pack(fill=tk.BOTH, expand=tk.YES)
+
+    def bind_redraw_events(self):
+        self.canvas.bind("<Configure>", self.retrieve_parameters_and_draw)
+        for widget in [self.func, self.left, self.right, self.a, self.b, self.c]:
+            widget.trace("w", self.retrieve_parameters_and_draw)
+
+    @staticmethod
+    def create_function_entry(master, functions):
+        Label(master, text='Функция', background=app['theme'])\
+            .grid(row=1, column=1, sticky=tk.E, padx=10, pady=5)
+        func = StringVar(master)
+        func.set(list(functions)[0])
+        func_entry = OptionMenu(master, func, *list(functions))
+        func_entry.configure(borderwidth=0)
+        func_entry.grid(row=1, column=2, sticky=tk.W + tk.E)
+        return func
+
+    @staticmethod
+    def create_real_entry(master, text, row, initial_value):
+        Label(master, text=text, background=app['theme'])\
+            .grid(row=row, column=1, sticky=tk.E, padx=10, pady=5)
+        var = DoubleVar(master)
+        var.set(initial_value)
+        left_entry = Entry(master, textvariable=var)
+        left_entry.grid(row=row, column=2, sticky=tk.W)
+        return var
+
+    def retrieve_parameters_and_draw(self, *args):
+        try:
+            a, b, c = self.a.get(), self.b.get(), self.c.get()
+            left, right = self.left.get(), self.right.get()
+            function = self.functions[self.func.get()]
+            width, height = self.canvas.winfo_width(), self.canvas.winfo_height()
+            self.async_draw(width, height, left, right, lambda x: function(x, a, b, c))
+        except TclError:
+            self.show_message("Некорректные параметры")
+
+    def async_draw(self, width, height, left, right, func):
+        def draw():
             self.drawing = True
             self.canvas.delete('all')
             self.canvas.pack_forget()
+            drawer = Drawer(width, height, left, right, func, self.draw_line)
             try:
-                draw(*args)
-            except Exception:
-                pass
-            self.canvas.pack(fill=BOTH, expand=YES)
+                drawer.draw()
+            except (ArithmeticError, ValueError, TypeError):
+                self.show_message("Функция не определена")
+            self.canvas.pack(fill=tk.BOTH, expand=tk.YES)
             self.drawing = False
 
         if not self.drawing:
-            Thread(target=draw_with_flags).start()
+            Thread(target=draw).start()
 
+    def show_message(self, text):
+        self.canvas.delete('all')
+        width, height = self.canvas.winfo_width(), self.canvas.winfo_height()
+        self.canvas.create_text(width / 2, height / 2, text=text)
+
+    def draw_line(self, x1, y1, x2, y2, color):
+        self.canvas.create_line(x1, y1, x2, y2, fill=color)
