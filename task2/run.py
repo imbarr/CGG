0f1bc076efe9614455e8c354da86201356d9dffa
Imbar <imbrkvskh@gmail.com>
2021-03-14 20:21:44 +0500

    task2 update

--- task2/run.py
+++ task2/run.py
@@ -12,26 +12,31 @@ class App(Tk):
     def plot(self, x, y):
         self.canvas.create_line(960 + x, 540 - y, 961 + x, 540 - y, fill="black")
 
-    def plot_all(self, x, y, b, c):
-        self.plot(x, y)
-        self.plot(y - c - b, x + b + c)
-        self.plot(- 2*b - x, 2*c - y)
-        self.plot(c - y - b, - x - b + c)
+    def plot_all(self, x, y, a, b, c):
+        X = x + b
+        Y = y - c
+        invert = 1
+        if a < 0:
+            invert = -1
+        self.plot(X - b, invert*Y + c)
+        self.plot(Y - b, invert*X + c)
+        self.plot(-X - b, c - invert*Y)
+        self.plot(-Y - b, -invert*X + c)
 
     def draw_hyperbola_slow(self, a, b, c):
-        y = int(math.sqrt(a))
+        y = int(math.sqrt(abs(a)))
         for x in range(y, 10000):
-            self.plot_all(x, y, b, c)
+            self.plot_all(x, y, a, b, c)
             delta = 2*x*y - 2*x*c + 2*y - 2*c + 2*b*y - 2*b*c - b - 1 - x - 2*a
             if delta > 0:
                 y -= 1
 
     def draw_hyperbola_fast(self, a, b, c):
-        x0 = int(math.sqrt(a)) - b
-        y = int(math.sqrt(a)) + c
-        delta = -math.sqrt(a)
+        x0 = int(math.sqrt(abs(a))) - b
+        y = int(math.sqrt(abs(a))) + c
+        delta = -math.sqrt(abs(a))
         for x in range(int(x0), int(x0) + 1920):
-            self.plot_all(x, y, b, c)
+            self.plot_all(x, y, a, b, c)
             if delta > 0:
                 y -= 1
                 delta += 2*y - 2*x - 2*c - 2*b - 5
@@ -43,5 +48,5 @@ if __name__ == '__main__':
     app = App()
     app.canvas.create_line(960, 0, 960, 1080, fill="red")
     app.canvas.create_line(0, 540, 1920, 540, fill="red")
-    app.draw_hyperbola_fast(10000, 200, 200)
+    app.draw_hyperbola_fast(-10000, 100, 100)
     app.mainloop()
